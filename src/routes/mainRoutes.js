4a154a72d9038bcecd1e382395ebd95521d37a8a
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2024-12-17 23:46:57 -0300

    agrego el resto de las localidades

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -5,5 +5,15 @@ const controller = require("../controllers/mainController");
 // aclaro a que controlador me estoy refiriendo
 router.get("/", controller.index);
 router.get("/villaurquiza", controller.villaurquiza);
+router.get("/belgrano", controller.belgrano);
+router.get("/belgranor", controller.belgranor);
+router.get("/chacarita", controller.chacarita);
+router.get("/coghlan", controller.coghlan);
+router.get("/colegiales", controller.colegiales);
+router.get("/nunez", controller.nunez);
+router.get("/saavedra", controller.saavedra);
+router.get("/sanisidro", controller.sanisidro);
+router.get("/vicentelopez", controller.vicentelopez);
+router.get("/villadelparque", controller.villadelparque);
 
 module.exports = router;
