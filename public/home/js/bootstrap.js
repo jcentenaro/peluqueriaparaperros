09be5bb5a3618a1e6cf9a26c6ffdbc5b71a6578a
Juan Centenaro <87022082+jcentenaro@users.noreply.github.com>
2024-12-18 21:32:50 -0300

    minimizo css y js 1

--- public/home/js/bootstrap.js
+++ public/home/js/bootstrap.js
@@ -16,15 +16,6 @@ if (typeof jQuery === 'undefined') {
   }
 }(jQuery);
 
-/* ========================================================================
- * Bootstrap: transition.js v3.3.7
- * http://getbootstrap.com/javascript/#transitions
- * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
-
-
 +function ($) {
   'use strict';
 
